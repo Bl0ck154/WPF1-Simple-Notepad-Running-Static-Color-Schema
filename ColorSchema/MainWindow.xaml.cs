1a2232ff939e229a2b9914929b3f3ad36fb9530c
Dell <Dell@DELL-PC>
2018-05-08 12:00:16 +0300

    Add project files.

---
+++ ColorSchema/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorSchema
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, string> memory = new Dictionary<string, string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtboxName.GotFocus += TxtboxName_GotFocus;
+            txtboxName.LostFocus += TxtboxName_LostFocus;
+            txtboxName.TextChanged += TxtboxName_TextChanged;
+            txtboxColor.GotFocus += TxtboxName_GotFocus;
+            txtboxColor.LostFocus += TxtboxColor_LostFocus;
+        }
+        private void TxtboxColor_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtboxName.Text))
+                txtboxColor.Text = "Enter string color like Red, Blue, Green...";
+        }
+
+        private void TxtboxName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtboxName.Text))
+                txtboxName.Text = "Enter name here...";
+        }
+
+        private void TxtboxName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            (sender as TextBox).Text = "";
+        }
+
+        private void TxtboxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ChangeBackground();
+        }
+        private void ChangeBackground()
+        {
+            string value;
+            if (memory.TryGetValue(txtboxName.Text, out value))
+            {
+                try
+                {
+                    grid.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(value));
+                }
+                catch { }
+            }
+            else
+                grid.Background = Background;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            memory[txtboxName.Text] = txtboxColor.Text;
+            ChangeBackground();
+        }
+    }
+}
---
+++ RunningStatic/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RunningStatic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            this.MouseMove += MainWindow_MouseMove;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ClearValue(SizeToContentProperty);
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point curPos = e.GetPosition(this);
+            double left = Canvas.GetLeft(ClickMeButton);
+            double top = Canvas.GetTop(ClickMeButton);
+            if (curPos.X >= left-15 && curPos.X <= left+ClickMeButton.ActualWidth + 15 
+                && curPos.Y >= top - 15 && curPos.Y <= top+ ClickMeButton.ActualHeight + 15)
+            {
+                if (left <= 0 || left + ClickMeButton.ActualWidth > field.ActualWidth)
+                    left = field.ActualWidth /2;
+              
+                if(top <= 0 || top + ClickMeButton.ActualHeight > field.ActualHeight)
+                    top = field.ActualHeight /2;
+
+                if (curPos.X >= left + ClickMeButton.ActualWidth / 2)
+                    Canvas.SetLeft(ClickMeButton, left - 2);
+                else if(curPos.X < left + ClickMeButton.ActualWidth / 2)
+                    Canvas.SetLeft(ClickMeButton, left + 2);
+                else if (curPos.Y >= top + ClickMeButton.ActualHeight / 2)
+                    Canvas.SetTop(ClickMeButton, top - 2);
+                else
+                    Canvas.SetTop(ClickMeButton, top + 2);
+            }
+            Title = $"left:{left} top:{top} cursor:{curPos.ToString()} actualHeight: {field.ActualHeight}";
+        }
+    }
+}
